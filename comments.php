309cd3e38dde58bfda0ade10e8adfb541931d337
Apessa <39739942+Apessa@users.noreply.github.com>
2018-05-29 20:31:57 +0000

    Add files via upload

--- comments.php
+++ comments.php
@@ -1,33 +1,71 @@
 <?php
-	// DISPLAY COMMENTS IF COMMENTS ARE OPENED
-    if ( comments_open() ) {
-		echo '<div class="comments">',
-			 '<h2>';
-		_e('Comments', 'wikiwp');
-		echo '</h2>';
-		if ( have_comments() ) {
-			// this is displayed if there are comments
-			echo '<h3>';
-			_e('This post currently has', 'wikiwp');
-			echo '&nbsp;';
-			comments_number( __('no responses','wikiwp'), __('one response', 'wikiwp'), __('% responses','wikiwp') );
-			echo '</h3>',
-				 '<ul class="commentlist">';
-			wp_list_comments();
-			echo '</ul>',
-				 '<div class="comment-nav">',
-				 '<div class="alignleft">';
-			previous_comments_link();
-			echo '</div>',
-				 '<div class="alignright">';
-			next_comments_link();
-			echo '</div>',
-				 '</div>';
-		} else {
-			// this is displayed if there are no comments so far
-			_e('So empty here ... leave a comment!', 'wikiwp');
-		}
-		// load comment form
-		comment_form();
-		echo '</div>'; // end of .content
+/**
+ * @package WordPress
+ * @subpackage Theme_Compat
+ * @deprecated 3.0.0
+ *
+ * This file is here for backward compatibility with old themes and will be removed in a future version
+ *
+ */
+_deprecated_file(
+	/* translators: %s: template name */
+	sprintf( __( 'Theme without %s' ), basename( __FILE__ ) ),
+	'3.0.0',
+	null,
+	/* translators: %s: template name */
+	sprintf( __( 'Please include a %s template in your theme.' ), basename( __FILE__ ) )
+);
+
+// Do not delete these lines
+	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+		die ('Please do not load this page directly. Thanks!');
+
+	if ( post_password_required() ) { ?>
+		<p class="nocomments"><?php _e('This post is password protected. Enter the password to view comments.'); ?></p>
+	<?php
+		return;
 	}
+?>
+
+<!-- You can start editing here. -->
+
+<?php if ( have_comments() ) : ?>
+	<h3 id="comments">
+		<?php
+			if ( 1 == get_comments_number() ) {
+				/* translators: %s: post title */
+				printf( __( 'One response to %s' ),  '&#8220;' . get_the_title() . '&#8221;' );
+			} else {
+				/* translators: 1: number of comments, 2: post title */
+				printf( _n( '%1$s response to %2$s', '%1$s responses to %2$s', get_comments_number() ),
+					number_format_i18n( get_comments_number() ),  '&#8220;' . get_the_title() . '&#8221;' );
+			}
+		?>
+	</h3>
+
+	<div class="navigation">
+		<div class="alignleft"><?php previous_comments_link() ?></div>
+		<div class="alignright"><?php next_comments_link() ?></div>
+	</div>
+
+	<ol class="commentlist">
+	<?php wp_list_comments();?>
+	</ol>
+
+	<div class="navigation">
+		<div class="alignleft"><?php previous_comments_link() ?></div>
+		<div class="alignright"><?php next_comments_link() ?></div>
+	</div>
+ <?php else : // this is displayed if there are no comments so far ?>
+
+	<?php if ( comments_open() ) : ?>
+		<!-- If comments are open, but there are no comments. -->
+
+	 <?php else : // comments are closed ?>
+		<!-- If comments are closed. -->
+		<p class="nocomments"><?php _e('Comments are closed.'); ?></p>
+
+	<?php endif; ?>
+<?php endif; ?>
+
+<?php comment_form(); ?>
